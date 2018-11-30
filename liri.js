5487fe69ef2c93a6e59fed3667fd385cd7195c7f
e-g-jackson <42052471+e-g-jackson@users.noreply.github.com>
2018-11-30 17:35:43 -0600

    **Refinement intensifies**

--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
 //global variables and npm
 require('dotenv').config();
+var fs = require('file-system')
 var axios = require('axios');
 var SpotifyAPI = require('node-spotify-api');
 var input = process.argv[2];
 var input2 = process.argv[3];
 //verify inputs
 console.log('input = ', input, ' input2 = ', input2);
-//BandsInTown
-if(input == 'concert-this'){
+var BITFxn = function(){
     var BIT = 'https://rest.bandsintown.com/artists/' + input2 + '/events?app_id=codingbootcamp';
     axios.get(BIT).then(function(response){
         var rsp = response.data;
@@ -37,50 +37,47 @@ if(input == 'concert-this'){
         console.log(error.response.data)
     })
 }
-//Spotify
-else if(input == 'spotify-this-song'){
+var spotifyFxn = function(){
     var clientId = process.env.SPOTIFY_ID;
     var clientSecret = process.env.SPOTIFY_SECRET;
     var spotify = new SpotifyAPI({
         id: clientId,
         secret: clientSecret,
-      });
+    });
     if (!input2){
         input2 = 'The Sign Ace of Base'
-    }   
+        }   
     spotify.search({
         type: 'track',
         query: input2,
-    },
-        function(error, data){
-            if (!error){
-                var info = data.tracks.items;
-                for( var i = 0; i < 5; i++){
-                    if (info[i] != undefined){
-                        var results = {
-                            'artist': info[i].artists[0].name,
-                            'song': info[i].name,
-                            'prvURL': info[i].preview_url,
-                            'album': info[i].album.name,
-                        };
-                        console.log('---------------------------');
-
-                        console.log('Song Title: ' + results.song);
-                        console.log('Album Title: ' + results.album);
-                        console.log('Artist: ' + results.artist);
-                        console.log('Preview: ' + results.prvURL);
-                        
-                        console.log('---------------------------');
+    }, function(error, data){
+        if (!error){
+            var info = data.tracks.items;
+            for( var i = 0; i < 5; i++){
+                if (info[i] != undefined){
+                    var results = {
+                        'artist': info[i].artists[0].name,
+                        'song': info[i].name,
+                        'prvURL': info[i].preview_url,
+                        'album': info[i].album.name,
                     };
+                    console.log('---------------------------');
+
+                    console.log('Song Title: ' + results.song);
+                    console.log('Album Title: ' + results.album);
+                    console.log('Artist: ' + results.artist);
+                    console.log('Preview: ' + results.prvURL);
+                    
+                    console.log('---------------------------');
                 };
-            }
-            else{
-                console.log(error);
-            }
-        })
-}
-//OMDB
-else if (input == 'movie-this'){
+            };
+        }
+        else{
+            console.log(error);
+        }
+    });
+};
+var OMDBFxn = function(){
     if (!input2){
         input2 = 'Mr.Nobody'
     }   
@@ -103,8 +100,32 @@ else if (input == 'movie-this'){
         console.log(error);
         console.log(error.rsp);
     })
-}
-//Wildcard fxn
-else if (input == 'do-what-it-says'){
+};
+var decider = function(){
+    //BandsInTown
+    if(input == 'concert-this'){
+        BITFxn();
+    }
+    //Spotify
+    else if(input == 'spotify-this-song'){
+        spotifyFxn();
+    }
+    //OMDB
+    else if (input == 'movie-this'){
+        OMDBFxn();
+    }
+    //Wildcard fxn
+    else if (input == 'do-what-it-says'){
+        fs.readFile('random.txt', 'utf8', function(error, data){
+            if(error){
+            return console.log(error); 
+            }
+            var dataArr = data.split(',');
+            input = dataArr[0];
+            input2 = dataArr[1];
+            decider();
+        });
+    }
+};
 
-}
+decider();
